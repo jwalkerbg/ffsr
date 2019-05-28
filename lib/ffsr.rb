4f04aadb4c6009de15d5f91430ead081ee305e2d
jwalkerbg <i_cenov@botevgrad.com>
2019-05-28 22:32:22 +0300

    RT #3568: Work on @Mainclass#main@ We have now working program.

--- lib/ffsr.rb
+++ lib/ffsr.rb
@@ -21,16 +21,32 @@ module Ffsr
       # primary parse of the commandline options
       begin
         options = OptParse.parse(ARGV)
-        #pp options
-        #raise 'No kind of the input files was given (-k option). Please run \'smde -h\'' if options[:kind] == nil
-      rescue => e
-        puts e
+        raise CommandLineErrorHandler.new('No program and/or files are given') if ARGV.size < 2
+      rescue CommandLineErrorHandler => e
+        e.printMessage
         options[:opterrors] += 1
       end
 
       ConditionalExit(options)
 
-    end
+      program, *files = ARGV
+
+# check existence of the program
+      begin
+        raise CommandLineErrorHandler.new('The program executable does not exists') if File.file?(program) == false
+      rescue
+        e.printMessage
+        options[:opterrors] += 1
+      end
+
+      ConditionalExit(options)
+
+# work here
+      files.each do |f|
+        puts program + ' ' + f if options[:verbose] != nil && options[:verbose] == true
+        system(program,f) if options[:dryrun] == nil || options[:dryrun] != true
+      end
+    end # main
 
 # ConditionalExit
 # This function exits program if there were command line errors
@@ -40,6 +56,6 @@ module Ffsr
         exit OPTION_ERROR
       end
     end
-  end
+  end   # Mainclass
 
 end # end module
