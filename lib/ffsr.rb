c4a43d64fa803e22696670ceeb0bb7d9970ee2e1
jwalkerbg <i_cenov@botevgrad.com>
2019-05-28 16:15:31 +0300

    RT #3568: Added command 'ffsr', main module 'Ffsr' and main class 'Mainclass'. Changed version to 1.0.1

--- lib/ffsr.rb
+++ lib/ffsr.rb
@@ -1,6 +1,24 @@
 require "ffsr/version"
+require 'optparse'
+
 
 module Ffsr
   class Error < StandardError; end
   # Your code goes here...
+
+# applcation definition
+  APPNAME = 'ffsr'
+  IDSW_NUMBER = 109218
+
+  NORMAL_EXIT = 0
+  OPTION_ERROR = 1
+
+# main program begins here
+  class Mainclass
+    def main
+      puts APPNAME + ' ' + VERSION
+    end
+  end
+
+
 end
